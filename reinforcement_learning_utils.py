cdb42c029d723e5e194c8efd627ed584d6a4b828
kb.bk <karim.belhadj@ledr.io>
2022-12-20 11:46:18 +0100

    cleaning of util

--- reinforcement_learning_utils.py
+++ reinforcement_learning_utils.py
@@ -10,7 +10,7 @@ def valueIteration(
 ):
 	Vk = np.zeros(nb_state)
 	Vk1 = np.zeros(nb_state)
-	for i in range(max_iterations):
+	for _ in range(max_iterations):
 		for s in range(nb_state):
 			L = np.zeros(nb_action)
 			for a in range(nb_action):
@@ -38,10 +38,9 @@ def policyIteration(
 	Vk1 = np.zeros(nb_state)
 	policy = np.ones(nb_state)
 	policy = policy.astype('int')
-	policy_stable = False
 	while(not policy_stable):
 		policy_stable = True
-		for i in range(max_iterations):
+		for _ in range(max_iterations):
 			for s in range(nb_state):
 				L = np.zeros(nb_action)
 				value = 0
@@ -68,49 +67,53 @@ def policyIteration(
 	return Vk1
 
 
-def epsGreedy(epsilon, q):
-	a = np.argmax(q)
+def epsGreedy(
+	epsilon       : float,
+	vector_values : np.array
+):
 	if np.random.rand() < epsilon:
-		a = np.random.randint(len(q))
-	return a
+		action = np.random.randint(len(vector_values))
+		return action
+	return np.argmax(vector_values)
 
 
 def sarsa(
-	n_episode : int,
-	gamma     : float,
-	alpha     : float,
-	epsilon   : float,
-	n_states  : int,
-	n_action  : int,
+	n_episode    : int,
+	gamma        : float,
+	alpha        : float,
+	epsilon      : float,
+	n_states     : int,
+	n_action     : int,
+	reward_decay : float = 0.001,
 ):
 	reward_per_episode = []
-	q = np.zeros((n_states, n_action))
-	for episode in range(n_episode):
+	vector_values = np.zeros((n_states, n_action))
+
+	for _ in range(n_episode):
 		observation = env.reset()
-		action = epsGreedy(epsilon, q[observation, :])
+		action = epsGreedy(epsilon, vector_values[observation, :])
 		sum_reward = 0
-		for t in range(3000):
+		for _ in range(3000):
 			observationP, reward, done, _ = env.step(action)
 			sum_reward += reward
+
 			if (done and reward == 0):
 				reward = 0
 			elif(not done):
-				reward = -0.001
-			actionP = epsGreedy(epsilon, q[observationP,:])
-			#if done:
-			#  tmp = alpha * (reward - q[observation, action])
-			#else:
-			tmp = alpha * (reward + gamma * q[observationP, actionP] - q[observation, action])
-			q[observation, action] = q[observation, action] + tmp
+				reward = -reward_decay
 
-			action = actionP
+			actionP = epsGreedy(epsilon, vector_values[observationP,:])
+			tmp     = alpha * (reward + gamma *  vector_values[observationP, actionP] - vector_values[observation, action])
+			vector_values[observation, action] = vector_values[observation , action ] + tmp
+
+			action      = actionP
 			observation = observationP
 			sum_reward += reward
 
 			if done:
 				break
 		reward_per_episode.append(sum_reward)
-	return q, reward_per_episode
+	return vector_values, reward_per_episode
 
 
 
@@ -121,33 +124,39 @@ def q_learning(
 	epsilon   : float,
 	n_states  : int,
 	n_action  : int,
+	reward_decay : float = 0.001,
 ):
 	reward_per_episode = []
-	q = np.zeros((n_states, n_action))
-	for episode in range(n_episode):
+	vector_values = np.zeros((n_states, n_action))
+
+	for _ in range(n_episode):
 		observation = env.reset()
-		action = epsGreedy(epsilon, q[observation, :])
+		action = epsGreedy(epsilon, vector_values[observation, :])
 		sum_reward = 0
-		for t in range(3000):
+
+		for _ in range(3000):
 			observationP, reward, done, _ = env.step(action)
 			sum_reward += reward
+
 			if (done and reward == 0):
 				reward = 0
 			elif(not done):
-				reward = -0.001
-			actionP = epsGreedy(epsilon, q[observationP,:])
-			if done:
-				tmp = alpha * (reward	- q[observation, action])
-			else:
-				mx = np.max(q[observationP,:])
-				tmp = alpha * (reward + gamma * mx - q[observation, action])
-			q[observation, action] = q[observation, action] + tmp
+				reward = -reward_decay
 
-			action = actionP
+			actionP = epsGreedy(epsilon, vector_values[observationP,:])
+			if done:
+				tmp = alpha * (reward	- vector_values[observation, action])
+			else:
+				mx = np.max(vector_values[observationP,:])
+				tmp = alpha * (reward + gamma * mx - vector_values[observation, action])
+			
+			vector_values[observation, action] = vector_values[observation, action] + tmp
+
+			action      = actionP
 			observation = observationP
 			sum_reward += reward
 
 			if done:
 				break
 		reward_per_episode.append(sum_reward)
-	return q, reward_per_episode
+	return vector_values, reward_per_episode
